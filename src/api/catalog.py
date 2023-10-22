67c038e852506abebf4e97ab820115e05927e4ea
gibsonhooper25 <gdhooper@calpoly.edu>
2023-10-22 16:32:24 -0700

    fix catalog showing 0 quantity

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -15,15 +15,16 @@ def get_catalog():
     with db.engine.begin() as connection:
         result = connection.execute(
             sqlalchemy.text("""SELECT sku, name, SUM(potion_transactions.potion_quantity_change) AS quantity, price, red_ml, green_ml, blue_ml, dark_ml FROM potion_transactions JOIN potions ON potions.id=potion_transactions.potion_id
-                                WHERE quantity > 0  GROUP BY sku, name, quantity, price, red_ml, green_ml, blue_ml, dark_ml ORDER BY quantity desc LIMIT 6"""))
+                                 GROUP BY sku, name, price, red_ml, green_ml, blue_ml, dark_ml ORDER BY quantity desc LIMIT 6"""))
     for potion in result:
-        catalog.append({
-            "sku": potion.sku,
-            "name": potion.name,
-            "quantity": potion.quantity,
-            "price": potion.price,
-            "potion_type": [potion.red_ml, potion.green_ml, potion.blue_ml, potion.dark_ml]
-        })
+        if potion.quantity > 0:
+            catalog.append({
+                "sku": potion.sku,
+                "name": potion.name,
+                "quantity": potion.quantity,
+                "price": potion.price,
+                "potion_type": [potion.red_ml, potion.green_ml, potion.blue_ml, potion.dark_ml]
+            })
     print("SHOWN CATALOG = ")
     print(catalog)
     return catalog
